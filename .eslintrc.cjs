48c16cb0c9a4fd247a92449abeebe0bf505a6a8d
Robi Alisandi <robi.alisandi@sekolahmu.co.id>
2023-09-23 23:22:38 +0700

    feat: initial commit 🚀

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  rules: {}
+}
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="logo">🤩</div>
+      <h1>Pocket Quran is coming soon</h1>
+    </>
+  )
+}
+
+export default App
