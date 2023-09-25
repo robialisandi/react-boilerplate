a9e0c0e001b786873c14a82b39814e7f166aaa18
Robi Alisandi <robi.alisandi@sekolahmu.co.id>
2023-09-25 19:30:20 +0700

    docs: change readme

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import './App.css'
 function App() {
   return (
     <>
-      <div className="text-9xl">🤩</div>
-      <h1 className="text-5xl font-bold">Pocket Quran is coming soon</h1>
+      <div className="text-9xl">🚀</div>
+      <h1 className="text-5xl font-bold">React Boilerplate</h1>
     </>
   )
 }
